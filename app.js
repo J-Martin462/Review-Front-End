aee8fe55b205249e48ee9cc846c8e054c16055b4
Jesse Martin del Campo <904225@H2WHK129Q6NW.tld>
2022-08-29 08:07:53 -0500

    just a counter issue left and some css

--- app.js
+++ app.js
@@ -26,3 +26,78 @@ function sentences(verb, adverb, person, adj, place){
 }
 
 console.log (sentences())
+
+var a = [1, 2, 4, 9]
+
+var wordArr = [
+    {
+        word: "dog",
+        hint: "company you keep at home"
+    },
+    {
+        word: "place",
+        hint: "were you are anytime"
+    },
+    {
+        word: "tree",
+        hint: "it grouws on the ground"
+    },
+    {
+        word: "computer",
+        hint: "youre using it right now"
+    }
+]
+
+var rN = Math.floor(Math.random() * wordArr.length);
+
+var word = wordArr[rN].word;
+var hint = wordArr[rN].hint;
+
+document.getElementById("word").innerHTML= word;
+document.getElementById("hint").innerHTML = hint;
+
+var w = document.getElementById("word")
+
+var correct = []
+
+var counter = word.length + 2;
+
+document.getElementById("wrong").innerHTML = counter;
+
+var splitWord = word.split("");
+
+var emptySplitWord = [];
+
+console.log(splitWord)
+
+for (i = 0; i < splitWord.length; i++) {
+    emptySplitWord[i] = "_"
+    console.log(emptySplitWord)
+    document.getElementById("word").innerHTML = emptySplitWord.join(" ")
+
+}
+
+function enterLetter() {
+    var letter = document.getElementById("letter").value;
+
+    for ( x = 0; x < splitWord.length; x++) {
+        if (letter == splitWord[x]){
+            emptySplitWord[x] = letter;
+            w.innerHTML = emptySplitWord.join("");
+            document.getElementById("letter").value = "";
+            correct.push(1)
+        } else {
+            document.getElementById("letter").value = "";
+        }
+    }
+
+    if (correct.length == emptySplitWord.length) {
+        alert("THE EMD")
+    } else if (counter > 1) {
+        counter -= 1;
+        document.getElementById("wrong").value = counter;
+    } else if (counter == 1) {
+        alert("You Lost")
+    }
+
+}
